2daaac2bed3b1a8258f7dedf9876ac2084207768
Schafer98 <schafer.cunningham@ttu.edu>
2021-03-12 14:03:18 -0600

    Added the ability to go to the RegisterUser Activity by clicking the SignUp Button.  Also changed the UI to fit across all devices

--- app/src/main/java/com/animusgames/drinkinbuddy/MainActivity.kt
+++ app/src/main/java/com/animusgames/drinkinbuddy/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.animusgames.drinkinbuddy
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -9,9 +10,29 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var DrinkingBuddyText: TextView
+    private lateinit var LoginUsernameEditText: EditText
+    private lateinit var LoginPasswordEditText: EditText
+    private lateinit var LoginButton: Button
+    private lateinit var SignupButton: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        //Defining all of the Objects on the login page
+        DrinkingBuddyText = findViewById(R.id.txtDrinkinBuddy)
+        LoginUsernameEditText = findViewById(R.id.editTextUsername)
+        LoginPasswordEditText = findViewById(R.id.editTextPassword)
+        LoginButton = findViewById(R.id.btnLogin)
+        SignupButton = findViewById(R.id.btnSignUp)
+
+        SignupButton.setOnClickListener {
+            val intent = Intent(this, RegisterUser::class.java)
+            startActivity(intent)
+        }
     }
     
 }
