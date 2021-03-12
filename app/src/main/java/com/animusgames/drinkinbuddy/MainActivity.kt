272c584f422972e107e3053aa587d30b996d7c3a
Rahil Patel <patelrahil15@gmail.com>
2021-03-12 12:19:02 -0600

    Changed UI button colors

--- app/src/main/java/com/animusgames/drinkinbuddy/MainActivity.kt
+++ app/src/main/java/com/animusgames/drinkinbuddy/MainActivity.kt
@@ -9,12 +9,9 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
-
     
 }
